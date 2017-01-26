31b2a9c8add113b3616376e7e41cdf4ad1e24753
David Hunt <davidpaulhunt@gmail.com>
2017-01-26 09:45:10 -0500

    fixed regex test for imgs to be less greedy

--- snugglyTrump.js
+++ snugglyTrump.js
@@ -36,7 +36,7 @@ const kittyName = randomKittyName();
 const kittyImageUrl = randomKittyImageUrl();
 
 document.body.innerHTML = document.body.innerHTML
-.replace(/href=".*?"|<img.*src=".*?".*>|Donald John Trump|Donald J. Trump|Donald Trump|The Donald|DonaldJTrump|DonaldTrump|Donald|Mr. Trump|Trump/ig,
+.replace(/href=".*?"|<img.*?src=".*?".*?>|Donald John Trump|Donald J. Trump|Donald Trump|The Donald|DonaldJTrump|DonaldTrump|Donald|Mr. Trump|Trump/ig,
 (match) => {
   if (match.startsWith('href')) return match;
 
